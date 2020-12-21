f78c45dd41fe611c0cf53c6f4ed954537021dd5f
Lê Ngọc Tùng <55380075+tunglebron@users.noreply.github.com>
2020-12-21 10:19:14 +0700

    delete all

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('config/config.php');
-$session_userId = '';
-$_SESSION['userId'] = '';
-if (empty($session_userId) && empty($_SESSION['userId'])) {
-    $url = BASE_URL . 'index.php';
-    header("Location: $url");
-}
