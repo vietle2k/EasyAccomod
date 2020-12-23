3360eb2876fae90cd8ad191dacb83c3cc99b3b91
Lê Ngọc Tùng <55380075+tunglebron@users.noreply.github.com>
2020-12-24 01:27:07 +0700

    update

--- inc/navbar.php
+++ inc/navbar.php
@@ -11,8 +11,7 @@
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="availablehouse.php" class="nav-link">Available House</a></li>
-            <li class="nav-item"><a href="index.php#about" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="availablehouse.php" class="nav-link">Available House</a></li> 
             <?php 
               $msg = new Message();
               $allmsg = $msg->getMessage(Session::get('user_id'));
@@ -21,15 +20,60 @@
               }
             ?>  
             <?php
+              if(($_SERVER["REQUEST_METHOD"] === "POST") && isset($_POST['delmsg']) && isset($_GET['id']) ) {
+                $delmsg = $msg->tempDelmsg(Session::get('user_id'), $_GET['id']);
+              }
+            ?> 
+            <?php   
+            if(Session::get('login')==true && (Session::get('user') == 'owner' || Session::get('user') == 'renter')){ 
+              ?>
+              <li class="nav-item cta dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-ha>
+                  Noti <span class="badge badge-light"><?php echo count($allmsg); ?></span>
+                </a> 
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <?php 
+                    if ($msg) {
+                      foreach ($allmsg as $message) {
+                        ?>
+                          <a class="dropdown-item">
+                            <p style="font-size: 10px;">
+                            <?php echo $message['message']; ?>
+                              <form class="" action="<?php echo $_SERVER['PHP_SELF'].'?id='.$message['id']; ?>" method="post">
+                                <input onclick="<?php $url = "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s" : "") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                                Session::set('path',$url); ?> this.form.submit();" type="submit" name="delmsg" value="Delete">
+                              </form>
+                            </p> 
+                          </a>
+                        <?php }  
+                    } else { ?>
+                        <a class="dropdown-item">No new message</a> 
+                    <?php }
+                  ?>
+                </div>
+              </li>
+              <?php }           
+            ?> 
+            <?php
               if(Session::get('login') == true && (Session::get('user') == 'owner')) { ?>
                 <li class="nav-item cta"><a href="add_houserent.php" class="nav-link ml-lg-2"><span class="icon-upload"></span> Add For Rent </a></li>
-                <li class="nav-item cta cta-colored"><a href="owner_profile.php?owner_id=<?php echo Session::get('user_id'); ?>" class="nav-link"><span class="icon-user"></span> Profile </a></li>
-                <li class="nav-item cta"><a href="?logout=yes" class="nav-link"><span class=""></span> Logout </a></li>
+                <li class="nav-item cta dropdown cta-colored">
+                  <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-toggle="dropdown" aria-ha><span class="icon-user"></span> My Account </a>
+                  <div class="dropdown-menu" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item" href="owner_profile.php?owner_id=<?php echo Session::get('user_id'); ?>">Profile</a>
+                    <a class="dropdown-item" href="?logout=yes">Logout</a>
+                  </div>
+                </li>
                 <?php
               }
               if(Session::get('login') == true && (Session::get('user') == 'renter')) { ?>
-                  <li class="nav-item cta cta-colored"><a href="tenant_profile.php?tenant_id=<?php echo Session::get('user_id'); ?>" class="nav-link"><span class="icon-user"></span> Profile </a></li>
-                  <li class="nav-item cta"><a class="nav-link" href="?logout=yes"><span></span> Logout </a></li>
+                <li class="nav-item cta dropdown cta-colored">
+                  <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-toggle="dropdown" aria-ha><span class="icon-user"></span> My Account </a>
+                  <div class="dropdown-menu" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item" href="tenant_profile.php?tenant_id=<?php echo Session::get('user_id'); ?>">Profile</a>
+                    <a class="dropdown-item" href="?logout=yes">Logout</a>
+                  </div>
+                </li>    
                 <?php
               }
               if(Session::get('login') == false) { ?>
@@ -43,51 +87,7 @@
                 <li class="nav-item cta cta-colored"><a href="user_register.php" class="nav-link"><span class="icon-pencil"></span> Register</a></li>
                 <?php
               }
-            ?> 
-            <?php
-              if(($_SERVER["REQUEST_METHOD"] === "POST") && isset($_POST['delmsg']) && isset($_GET['id']) ) {
-                $delmsg = $msg->tempDelmsg(Session::get('user_id'), $_GET['id']);
-              }
-            ?>
-
-            <?php   
-
-      if(Session::get('login')==true && (Session::get('user') == 'owner' || Session::get('user') == 'tenant')){ ?>
-
-        <div class="notification_dropdown col-sm-5">
-          <div class="dropdown">
-            <button type="button" class="btn btn-primary" data-toggle="dropdown">
-              <i class="fas fa-bell"></i><sup><span style="margin-left:3px;" class="badge badge-light"><?php echo count($allmsg); ?></span></sup>
-            </button>
-            <div class="dropdown-menu">
-              <?php
-                if($msg){
-                  foreach ($allmsg as $message) {
-               ?>
-              <div class="dropdown-item">
-                <p><?php echo $message['message']; ?>
-                  <form class="" action="<?php echo $_SERVER['PHP_SELF'].'?id='.$message['id']; ?>" method="post">
-                    <input style="font-size:10px;padding:5px;" onclick="<?php $url = "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s" : "") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                    Session::set('path',$url); ?> this.form.submit();" type="submit" class="btn btn-danger small" name="delmsg" value="Delete">
-                  </form>
-                </p>
-              </div>
-            <?php } }else{ ?>
-              <p class="dropdown-item">No new message!</p>
-            <?php } ?>
-            </div>
-          </div>
-        </div>
-
-
-        <div class="user_dropdown col-sm-7">
-          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><?php echo Session::get('fname')." ".Session::get('lname');?></i> </button>
-          <div class="dropdown-menu">
-              <a class="dropdown-item" href="?logout=yes">Logout</a>
-          </div>
-        </div>
-      <?php }           
-            ?>       
+            ?>     
           </ul>
         </div>
       </div>
